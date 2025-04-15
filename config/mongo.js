14f4bd661c3490e2e1c40b4461d1d13191f775e4
Luisferrer02 <luisferrer2002@gmail.com>
2025-04-15 16:53:15 +0200

    configuracion para unit test

--- config/mongo.js
+++ config/mongo.js
@@ -1,15 +1,15 @@
-//config/mongo.js
-
 const mongoose = require("mongoose");
 
 const dbConnect = async () => {
-    try {
-      await mongoose.connect(process.env.DB_URI);
-      console.log("Conectado a la BD");
-    } catch (error) {
-      console.error("Error conectando a la BD:", error);
-    }
+  try {
+    // Si NODE_ENV es "test" se utiliza DB_URI_TEST, de lo contrario DB_URI
+    const dbUri = process.env.NODE_ENV === "test" ? process.env.DB_URI_TEST : process.env.DB_URI;
+    await mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    console.log("Conectado a la BD:", dbUri);
+  } catch (error) {
+    console.error("Error conectando a la BD:", error);
+    process.exit(1);
   }
-  
+};
 
 module.exports = dbConnect;
