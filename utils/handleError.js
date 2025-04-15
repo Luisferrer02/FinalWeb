48b66634494a1accdc56a3857c292a82e6b42cf0
Luisferrer02 <luisferrer2002@gmail.com>
2025-04-15 16:53:44 +0200

    handlers mas detallados

--- utils/handleError.js
+++ utils/handleError.js
@@ -1,6 +1,14 @@
 // utils/handleError.js
-const handleHttpError = (res, message, code = 403) => {
-  res.status(code).send(message)
-}
 
-module.exports = { handleHttpError }
+/**
+ * Envía una respuesta de error HTTP.
+ * @param {Response} res - Objeto respuesta de Express.
+ * @param {string} message - Mensaje o código de error.
+ * @param {number} code - Código HTTP (por defecto 403).
+ */
+const handleHttpError = (res, message, code = 403) => {
+  console.error("HTTP Error:", message);
+  res.status(code).json({ error: message });
+};
+
+module.exports = { handleHttpError };
--- utils/handleLogger.js
+++ utils/handleLogger.js
@@ -1,10 +1,18 @@
+// utils/handleLogger.js
+
 const { IncomingWebhook } = require("@slack/webhook");
-const webHook = new IncomingWebhook(process.env.SLACK_WEBHOOK);
+
+// Se espera que SLACK_WEBHOOK esté definido en el .env
+const webhook = new IncomingWebhook(process.env.SLACK_WEBHOOK);
+
+/**
+ * Objeto que actúa como stream y envía los mensajes a Slack.
+ */
 const loggerStream = {
   write: (message) => {
-    webHook.send({
-      text: message,
-    });
+    // agregar aquí algún formateo o filtrar mensajes
+    webhook.send({ text: message.trim() }).catch(err => console.error("Error enviando log a Slack:", err));
   },
 };
+
 module.exports = loggerStream;
