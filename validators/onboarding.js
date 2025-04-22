03a1fd095a58ea4df4d662817ef0f198397d470a
Luisferrer02 <luisferrer2002@gmail.com>
2025-04-22 17:43:12 +0200

    runValidateResults en onboarding

--- validators/onboarding.js
+++ validators/onboarding.js
@@ -1,19 +1,20 @@
 // validators/onboardingCompany.js
 const { check } = require("express-validator");
 const { validateResults } = require("../utils/handleValidator");
+const runValidateResults = (req, res, next) => validateResults(req, res, next);
 
 const validatorOnboardingCompany = [
   check("companyName").exists().notEmpty(),
   check("cif").exists().notEmpty(),
   check("address").exists().notEmpty(),
-  (req, res, next) => validateResults(req, res, next)
+  runValidateResults,
 ];
 
 const validatorOnboardingUser = [
   check("name").exists().notEmpty(),
   check("lastName").exists().notEmpty(),
   check("nif").exists().notEmpty(),
-  (req, res, next) => validateResults(req, res, next)
+  runValidateResults,
 ];
 
 module.exports = { validatorOnboardingCompany, validatorOnboardingUser };
