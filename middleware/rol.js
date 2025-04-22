762def83747f5b06ac6f28ad3ba260f976007526
Luisferrer02 <luisferrer2002@gmail.com>
2025-04-22 17:44:00 +0200

    refact

--- middleware/rol.js
+++ middleware/rol.js
@@ -1,19 +1,27 @@
 // middleware/rol.js
-const { handleHttpError } = require("../utils/handleError")
+const { handleHttpError } = require("../utils/handleError");
 
 const checkRol = (roles) => (req, res, next) => {
   try {
-    const { user } = req
-    const userRol = user.role
-    const checkValueRol = roles.includes(userRol)
-    if (!checkValueRol) {
-      handleHttpError(res, "NOT_ALLOWED", 403)
-      return
-    }
-    next()
-  } catch (err) {
-    handleHttpError(res, "ERROR_PERMISSIONS", 403)
-  }
-}
+    const user = req.headers.user ? JSON.parse(req.headers.user) : null;
 
-module.exports = checkRol
+    if (!user) {
+      handleHttpError(res, "ERROR_PERMISSIONS", 403); // Si no hay un usuario, devolvemos un error
+      return;
+    }
+
+    const userRol = user.role; // Obtenemos el rol del usuario
+    const checkValueRol = roles.includes(userRol); // Verificamos si el rol está permitido
+
+    if (!checkValueRol) {
+      handleHttpError(res, "NOT_ALLOWED", 403); // Si el rol no está permitido, devolvemos un error
+      return;
+    }
+
+    next(); // Si todo está bien, continuamos al siguiente middleware
+  } catch (err) {
+    handleHttpError(res, "ERROR_PERMISSIONS", 403); // En caso de error inesperado, devolvemos un error
+  }
+};
+
+module.exports = checkRol;
