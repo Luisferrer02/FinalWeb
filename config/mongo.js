7ed9791d7a50b03469f2482c9f6adbbd281bd283
Luisferrer02 <luisferrer2002@gmail.com>
2025-04-22 17:41:11 +0200

    config mongo

--- config/mongo.js
+++ config/mongo.js
@@ -1,10 +1,10 @@
+//config/mongo.js
 const mongoose = require("mongoose");
 
 const dbConnect = async () => {
   try {
-    // Si NODE_ENV es "test" se utiliza DB_URI_TEST, de lo contrario DB_URI
     const dbUri = process.env.NODE_ENV === "test" ? process.env.DB_URI_TEST : process.env.DB_URI;
-    await mongoose.connect(dbUri, { useNewUrlParser: true, useUnifiedTopology: true });
+    await mongoose.connect(dbUri);
     console.log("Conectado a la BD:", dbUri);
   } catch (error) {
     console.error("Error conectando a la BD:", error);
