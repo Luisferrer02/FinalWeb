e9319bb85a9f0dc18357a9ee22692c78a1e68b9f
Luisferrer02 <luisferrer2002@gmail.com>
2025-04-25 11:52:10 +0200

    TODO para credenciales mail - no funciona

--- controllers/mail.js
+++ controllers/mail.js
@@ -3,6 +3,8 @@ const { sendEmail } = require('../utils/handleMails');
 const { handleHttpError } = require('../utils/handleError');
 const { matchedData } = require('express-validator');
 
+//TODO Credenciales Email Google OAuth
+
 const send = async (req, res) => {
   try {
     const emailData = matchedData(req);
